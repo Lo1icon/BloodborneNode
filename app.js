22d82e164a878bc8c8278152e1050c7d1b813422
Lynn <buaa.zyh@gmail.com>
2017-04-06 20:20:20 +0800

    update node_modules

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 
 var index = require('./routes/index');
-var getFlow=require('./routes/getFlow');
-var intime=require('./routes/intime');
-var T=require('./routes/T');
+var getFlow = require('./routes/getFlow');
+var intime = require('./routes/intime');
+var T = require('./routes/T');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.set('view engine', 'html');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,26 +29,26 @@ app.use('/Bloodborne', index);
 
 app.use('/getFlow', getFlow);
 
-app.use('/intime',intime);
-app.use('/T',T);
+app.use('/intime', intime);
+app.use('/T', T);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 app.listen(8080);
 module.exports = app;
