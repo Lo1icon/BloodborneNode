45629f95c89901aee075349bd1f87c7a00c78b6e
朱耀华-电子政务部-北京 <zhuyaohua01@gridsum.com>
2018-10-17 16:57:02 +0800

    add deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  apps: [{
+    name: 'API',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: '58.87.119.77',
+      ref: 'origin/localhost',
+      repo: 'git@github.com:LynnScarlett/BloodborneNode.git',
+      path: '/project/pmtest/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev: {
+      user: 'ubuntu',
+      host: '58.87.119.77',
+      ref: 'origin/localhost',
+      repo: 'git@github.com:LynnScarlett/BloodborneNode.git',
+      path: '/project/pmtest/dev',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env: {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
